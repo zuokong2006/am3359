c527492bd6c4d8f9fdc2d7f0827625e6d706e456
zuokong2006 <zuokong2006@163.com>
2014-05-23 13:40:47 +0800

    1、提交hello入门程序。

---
+++ app/hello/hello.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+
+int main(void) {
+	struct tm stTime = {0};
+	int i;
+	
+	for(i = 0; i < 5; i++) {
+		printf("hello word:%d \n", i);
+	}
+}
