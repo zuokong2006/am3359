43c5482e94ffb637be7f265b736a19f8243900c1
zuokong2006 <zuokong2006@163.com>
2014-06-06 15:44:54 +0800

    1、修改完成bb_led驱动测试程序。

--- app/led/led.c
+++ app/led/led.c
@@ -8,43 +8,28 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv)
 {
-	int fd = 0;
-	int ledn = 0;
+	int fd = 0, i = 0, ret = 0;
 	
-	/* 输入检查 */
-	ledn = strtoul(argv[1], 0, 0);
-	if((3 != argc) || (3 < ledn))
-	{
-		printf("input param error!\r\n");
-		exit(1);
-	}
-	fd = open("/dev/leds", 2);
+	fd = open("/dev/bb_led", O_RDWR);
+	printf("fd=%d\r\n", fd);
 	if(0 > fd)
 	{
-		perror("open /dev/leds");
+		perror("open /dev/bb_led");
 		exit(1);
 	}
 	
-	if(0 == strcmp(argv[2], "on"))
+	for(i=0; i<10; i++)
 	{
-		printf("led%d = on\r\n", ledn);
-		printf("ioctl ret = %d\r\n", ioctl(fd, 1, ledn));
-	}
-	else if(0 == strcmp(argv[2], "off"))
-	{
-		printf("led%d = off\r\n", ledn);
-		printf("ioctl ret = %d\r\n", ioctl(fd, 0, ledn));
-	}
-	else
-	{
-		close(fd);
-		printf("input argv[2] error!\r\n");
-		exit(1);
+		ret = ioctl(fd, i%2);
+		printf("ioctl ret = %d\r\n", ret);
+		sleep(1);
 	}
 	
 	close(fd);
+	
 	return 0;
 }
